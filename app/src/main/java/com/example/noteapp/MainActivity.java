3a68c1af187f8c857cf97019c912754f8339691a
Hùng Tăng <minhhung290701@gmail.com>
2022-04-14 17:29:05 +0700

    NoteAppv1

---
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.noteapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FloatingActionButton fab;
+    Adapter adapter;
+    List<Model> notesList;
+
+    DatabaseClass databaseClass;
+    CoordinatorLayout coordinatorLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        fab = findViewById(R.id.fab);
+        coordinatorLayout = findViewById(R.id.layout_main);
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,AddNotesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        notesList = new ArrayList<>();
+
+        databaseClass = new DatabaseClass(this);
+        fetchAllNotesFromDatabase();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new Adapter(this,MainActivity.this, notesList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void fetchAllNotesFromDatabase() {
+        Cursor cursor = databaseClass.readAllData();
+        if (cursor.getCount() == 0) {
+            Toast.makeText(this, "No Data to show", Toast.LENGTH_SHORT).show();
+        } else {
+            while (cursor.moveToNext()) {
+                notesList.add(new Model(cursor.getString(0), cursor.getString(1), cursor.getString(2)));
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.searchbar);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setQueryHint("Search Notes");
+        SearchView.OnQueryTextListener listener = new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return true;
+            }
+        };
+        searchView.setOnQueryTextListener(listener);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.delete_all_notes) {
+            deleteAllNotes();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deleteAllNotes() {
+        DatabaseClass db = new DatabaseClass(MainActivity.this);
+        db.deleteAllNotes();
+        recreate();
+    }
+
+}
